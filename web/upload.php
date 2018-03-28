f7fefd950d670aa0e01ad904eac722ec4187406f
Flavio Medeiros <flavio.medeiros@slmandic.edu.br>
2018-03-28 17:57:51 -0300

    Sync

--- web/upload.php
+++ web/upload.php
@@ -1,10 +1,22 @@
 <?php
-	
-	$uploaddir = 'C:/Users/1510030927/Downloads/metrocamp-projeto-desenvimento-mobile/upload';
+
+	$uploaddir = './uploads/';
+
+	if(!file_exists($uploaddir))
+		mkdir($uploaddir);
+
 	$uploadfile = $uploaddir . basename($_FILES['file']['name']);
 
 	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
 	    echo "Arquivo válido e enviado com sucesso.\n";
 	} else {
 	    echo "Possível ataque de upload de arquivo!\n";
+	}
+
+
+	function dd($var){
+	  echo "<pre>";
+	  var_dump($var);
+	  echo "</pre>";
+	  exit();
 	}
